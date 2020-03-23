985779a69867f29ecc07ccaf0cf70b828a7fb112
Manu Pacheco <mpachecopal@gmail.com>
2020-03-23 18:38:32 +0100

    done es8

--- src/es8/index.js
+++ src/es8/index.js
@@ -23,3 +23,30 @@ console.log(values)
 const string = 'hello'
 console.log(string.padStart(7, 'hi'))
 console.log(string.padEnd(12, ' ---- '))
+
+//! asyn await
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+    true
+      ? setTimeout(() => resolve('Hello World!'), 3000)
+      : reject(new Error('test error'))
+  })
+}
+
+const hellowAsync = async () => {
+  const hello = await helloWorld()
+  console.log(hello)
+}
+
+hellowAsync()
+
+const anotherFunction = async () => {
+  try {
+    const hello = await helloWorld();
+    console.log(hello)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+anotherFunction()
