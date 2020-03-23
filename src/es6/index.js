56b10c49457987cebe86ba2ffcf72805b5139c80
Manu Pacheco <mpachecopal@gmail.com>
2020-03-23 17:24:12 +0100

    round two

--- src/es6/index.js
+++ src/es6/index.js
@@ -72,4 +72,53 @@ console.log(globalVar)
 
 const a = 'b'
 a = 'a'
-console.log(a) // no se puede asignar 'Assignment to constant variable.'
+// console.log(a) // no se puede asignar 'Assignment to constant variable.'
+
+//!objetos
+let name = 'name'
+let age = 29
+
+let obj = { name = name, age = age }
+console.log(obj)
+
+//es6
+let obj2 = { name, age }
+console.log(obj2)
+
+//! arrow functions
+const names = [
+  { name: 'manu', age: 29},
+  { name: 'thor', age: 1002}
+]
+
+let listOfNames = names.map(function (item) {
+  console.log(item.name)
+})
+
+//es6
+let listOfNames2 = names.map(item => console.log(item.name))
+
+const listOfNames3 = (name, age) => {
+  // code
+}
+
+const listOfNames4 = name => {
+  // code 
+}
+
+const square = num => num * num;
+
+//!promises
+const helloPromise = () => {
+  return new Promise((resolve, reject) => {
+    if(true) {
+      resolve('Hey!')
+    } else {
+      reject('Ups!')
+    }
+  })
+}
+
+helloPromise()
+  .then(response => console.log(response))
+  .catch(error => console.error(error))
